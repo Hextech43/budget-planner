30fbcf3bbcf1e83b2c73069501f48df44a5a9a3e
Hextech43 <oluwatoyine86@gmail.com>
2022-04-04 15:09:39 +0100

    budget planner

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Homepage from './expensetracker/Homepage'
+
+const App = () => {
+  return (
+    <Homepage/>
+    // <div>hi there</div>
+  )
+  
+  
+}
+
+export default App
